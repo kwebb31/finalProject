51ecb00f853d90095b6068a836fb7ecbdfc6c07e
TommyThai-02 <tthai13@horizon.csueastbay.edu>
2023-11-27 14:15:56 -0800

    Basic Client Structure
    Added the basic lines for a client connection to a server, localhost needs to be changed to the ip of the device we will run the server on

--- Client.java
+++ Client.java
@@ -1,1 +1,40 @@
+package finalProject;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class Client {
+	User user = null;
+	static boolean isLoggedIn;
+	static boolean isConnected;
+	static String host = "localhost";
+	static int port = 1234;
+	public static void main(String[] args) throws ClassNotFoundException {
+		
+		try (Socket s = new Socket(host,port)) {
+					isLoggedIn = false;
+					isConnected = true;
+	                OutputStream outputStream = s.getOutputStream();
+	                ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+	           
+	                InputStream inputStream = s.getInputStream();
+	                ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+	                
+	                System.out.println("Client is connected to Server. Select an Option: ");
+	                Scanner sc = new Scanner(System.in);
+	                do {
+	                	
+	                }while (isConnected == true);
+	                
+	                
+
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+}
