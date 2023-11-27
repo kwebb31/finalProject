da8518c7b27d9d23a7204be8032d9e2adcfbe03a
kwebb31 <121592902+kwebb31@users.noreply.github.com>
2023-11-27 05:03:08 -0800

    Update Message.java
    filled in Message.java. currently only missing the getMessageID() function.

--- Message.java
+++ Message.java
@@ -1,1 +1,49 @@
+package finalProject;
 
+import java.util.*;
+
+public class Message {
+	
+	static Integer messageId = 0;
+	Date messageCreated = new Date();
+	String messageString;
+	String messageSender;
+	String messageReciever;
+	MessageStatus messageStatus;
+	public Message(String messageString, String messageSender, String messageReciever, MessageStatus messageStatus) {
+		this.messageString = messageString;
+		this.messageSender = messageSender;
+		this.messageReciever = messageReciever;
+		SetID();
+		this.messageStatus = messageStatus;
+	}
+	
+	
+	
+	private void SetID() {
+		messageId++;
+
+	}
+	
+	
+	void updateMessageStatus(MessageStatus newStatus) {
+		messageStatus = newStatus;
+	}
+	
+	Date getMessageDate() {
+		return messageCreated;
+	}
+	String getMessageString() {
+		return messageString;
+	}
+	
+	
+	
+	MessageStatus getMessageStatus() {
+		return messageStatus;
+	}
+	Integer getMessageID() {
+		return messageId;
+	}
+	
+}
