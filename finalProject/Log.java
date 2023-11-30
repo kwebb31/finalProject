764350482ad84b6be0883ae1736a5cfd63f2a79e
JonVazquez <70725837+JonVazquez@users.noreply.github.com>
2023-11-29 19:47:03 -0800

    Update Log.java

--- finalProject/Log.java
+++ finalProject/Log.java
@@ -40,16 +40,34 @@ public class Log {
 		}
 	
 	//method that returns string of all logged messages with sender "x", separated by "\n"
-	public String filterLogsBySenderID(String x) {
+	public String filterLogsBySender(String x) {
 		//StringBuilder class, allows for a mutable sequence of characters
 		StringBuilder filteredLoggedMessages = new StringBuilder();
+		
 		for (String loggedMessage : loggedMessagesArray) {
 			// Split the message by comma and extract the sender
 			String[] parts = loggedMessage.split(",");
-			String messageSender = parts[0].trim().split("\\(")[0];
+			String messageSender = parts[0].trim().split(" ")[0];
+			//System.out.println(messageSender);
 			if (x.equalsIgnoreCase(messageSender)) {
 				filteredLoggedMessages.append(loggedMessage).append("\n");
             }
+		}
+		return filteredLoggedMessages.toString();
+	}
+	
+	//String date format is expected to be in "MM.dd.yy" EX: "11.05.23"
+	public String filterLogsByDate(String x) {
+		StringBuilder filteredLoggedMessages = new StringBuilder();
+		
+		for (String loggedMessage : loggedMessagesArray) {
+			//split message by comma and extract date
+			String[] parts = loggedMessage.split("\\)");
+			String dateString = parts[1].trim().split(",")[0];
+			//System.out.println(dateString);
+			if (x.equalsIgnoreCase(dateString)) {
+				filteredLoggedMessages.append(loggedMessage).append("\n");
+			}
 		}
 		return filteredLoggedMessages.toString();
 	}
@@ -58,11 +76,11 @@ public class Log {
 	//I decided to call date of when a message is logged instead of created. Can be changed later
 	public void addMessageToFile(Message message) {
 		//get date and time
-		Date currentDate = new Date();
+		Date messageDate = message.getMessageDate();
 		SimpleDateFormat dateFormat = new SimpleDateFormat("MM.dd.yy,HH:mm");
-		String formattedDate = dateFormat.format(currentDate);
+		String formattedDate = dateFormat.format(messageDate);
 		//format the string to be logged
-		String messageToLog = message.getMessageSender() +"(" + message.getMessageReceiver() + ")" 
+		String messageToLog = message.getMessageSender() + "," +  message.getMessageSenderUID() + "(" + message.getMessageReciever() + ")" 
 		+ formattedDate + "," + message.getMessageString();
 		//write to file on a new line
 		//THIS IS WHERE THE FILE NAME SHOULD BE CHANGED
@@ -86,9 +104,14 @@ public class Log {
         System.out.println(log.getAllLogs());
         
         //test filterLogsBySender
-        System.out.println("All messages from sender Joe:\n");
-        String filteredMessages = log.filterLogsBySenderID("JOE");
+        System.out.println("All messages from sender JOE:\n");
+        String filteredMessages = log.filterLogsBySender("JOE");
         System.out.println(filteredMessages);
+        
+        //test filterLogsByDate
+        System.out.println("All messages from date 11.28.23:\n");
+        String filteredDates = log.filterLogsByDate("11.28.23");
+        System.out.println(filteredDates);
         
         //create message object
         Message sampleMessage = new Message("Hello, how are you?", "Alice", "Bob", MessageType.TEXT, 1001, 1002);
