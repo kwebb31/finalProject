8503948144b6ef3b960f0d0541dd0bcc3d980a12
TommyThai-02 <tthai13@horizon.csueastbay.edu>
2023-12-01 00:17:03 -0800

    changes to adjust message construction

--- finalProject/Client.java
+++ finalProject/Client.java
@@ -5,8 +5,10 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+
 
 public class Client {
 	private User user = new User();
@@ -18,6 +20,7 @@ public class Client {
 	private ObjectOutputStream objectOutputStream;
 	private ObjectInputStream objectInputStream;
 	private int loginAttempt = 0;
+
 	public Client(){
 		try {
 			s = new Socket(host,port);
@@ -32,7 +35,7 @@ public class Client {
 	}
 	public Boolean login(String username, String pw) throws IOException, ClassNotFoundException {
 		System.out.println("login request sending");
-		Message Temp = new Message(username + ":" + pw,username,"Server",MessageType.LOGIN,0);
+		Message Temp = new Message(username + ":" + pw,username,"Server",MessageType.LOGIN);
 		objectOutputStream.writeObject(Temp);
 		Temp = (Message) objectInputStream.readObject();
 		if(Temp.getMessageString().equals("Unsuccessful")) {
@@ -50,22 +53,19 @@ public class Client {
 		System.out.println("Login Success!");
 		return isLoggedIn;
 	}
-	public void sendMessage(String msg, String sender, String receiver, String senderUID, String receiverUID ) throws IOException {
-		String[] parse = receiver.split(",");
-		String[] parse2 = receiverUID.split(",");
-		int count = 0;
-		for(String s : parse) {
-			Message Temp = new Message(msg, sender, s, MessageType.TEXT, Integer.parseInt(senderUID) ,Integer.parseInt(parse2[count]));
-			count++;
+	public void sendMessage(String msg, String sender, String receiver, String senderUID, ArrayList<Integer> receiverUID ) throws IOException {
+			System.out.println("Hello from send message");
+			Message Temp = new Message(msg, sender, receiver, MessageType.TEXT, Integer.parseInt(senderUID) ,receiverUID);
 			objectOutputStream.writeObject(Temp);
-		}
-	}
-	public void receiveMessage() throws ClassNotFoundException, IOException {
-		Message Temp = (Message) objectInputStream.readObject();
 		
 	}
+	public void receiveMessage() throws ClassNotFoundException, IOException {
+		System.out.println("Hello from receive msg");
+		Message Temp = (Message) objectInputStream.readObject();
+		System.out.println(Temp.toString());
+	}
 	public Boolean logout() throws IOException, ClassNotFoundException {
-		Message Temp = new Message("",user.userName,"Server",MessageType.LOGOUT,0);
+		Message Temp = new Message("",user.userName,"Server",MessageType.LOGOUT);
 		objectOutputStream.writeObject(Temp);
 		Temp = (Message) objectInputStream.readObject();
 		if(Temp.getMessageString().equals("Success")) {
@@ -78,8 +78,8 @@ public class Client {
 	}
 	
 	public String getUserDirectory() throws IOException, ClassNotFoundException {
-		System.out.println("login request sending");
-		Message userDirectory =  new Message(user.getUserName(), user.getUserName(), "Server",MessageType.DIRECTORY,0);
+		//System.out.println("login request sending");
+		Message userDirectory =  new Message(user.getUserName(), user.getUserName(), "Server",MessageType.DIRECTORY);
 		objectOutputStream.writeObject(userDirectory);
 		userDirectory = (Message) objectInputStream.readObject();
 		return userDirectory.getMessageString();
